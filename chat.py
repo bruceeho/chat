b36957ae0c330022cfd3be3ee6f4a99a15f98b90
Bruce Ho <mitch6198@gmail.com>
2024-10-08 14:52:41 +0800

    finish

--- chat.py
+++ chat.py
@@ -1,11 +1,11 @@
-
+#讀取檔案將內容記錄到lines這個清單
 def read_file(filename):
     lines = []
-    with open('input.txt','r',encoding='UTF-8-sig') as f:
+    with open('input.txt','r',encoding='UTF-8-sig') as f:#若出現\ufeff需要用uff-8-sig編碼格式
         for line in f:
             lines.append(line.strip())
     return lines
-
+#剖析內容，呈現為需求的格式
 def convert(lines):
     new = []
     person = None
@@ -19,13 +19,12 @@ def convert(lines):
         if person:
             new.append(person +'：' + line)
     return new
-
-
+#將內容寫入至檔案中
 def write_file(filename,lines):
 	with open(filename,'w') as f:
 		for line in lines:
 		    f.write(line + '\n')
-
+#主程式執行
 def main():
     lines = read_file('input.txt')
     lines = convert(lines)
